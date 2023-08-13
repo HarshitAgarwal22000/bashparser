2e5b4bc4f731605475e9c1ab19de311f8d5d4793
HarshitAgarwal22000 <86183161+HarshitAgarwal22000@users.noreply.github.com>
2023-08-14 01:47:40 +0530

    Update parser.sh

--- parser.sh
+++ parser.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 log_file="C:\Users\harsh\OneDrive\Desktop\access.log"
 todas=$(grep "$(date +'%Y/%b/%d')" "$log_file")
-highhost=$(awk -F ',' '{print $13}' "$log_file"|sort|uniq -c|sort -nr|head -n 1)
-totalstatus_code=$(awk -F ',' '{print $6}' "$log_file"|sort|uniq -c)
+highhost=$(awk -F ' ' '{print $13}' "$log_file"|sort|uniq -c|sort -nr|head -n 1)
+totalstatus_code=$(awk -F ' ' '{print $6}' "$log_file"|sort|uniq -c)
 #printing today stuff
 echo "Today"
 echo "$todas"
@@ -12,14 +12,14 @@ echo "$highhost"
 echo "requests per status code"
 echo "$totalstatus_code"
 #printed all codes
-top_upstream_ip=$(awk -F ',' '{print $8}' "$log_file" | sort | uniq -c | sort -nr | head -n 5)
-top_requests_by_host=$(awk -F ',' '{print $13}' "$log_file" |sort| uniq -c | sort -nr | head -n 5)
-top_requests_by_body_bytes_sent=$(awk -F ',' '{print $9}' "$log_file" | sort -n | tail -n 5)
-top_requests_by_path=$(awk -F ',' '{print $4}' "$log_file" | cut -d '/' -f 1,2 | sort | uniq -c | sort -nr | head -n 5)
-top_requests_by_response_time=$(awk -F ',' '{print $7}' "$log_file" | sort -n | tail -n 5)
+top_upstream_ip=$(awk -F ' ' '{print $8}' "$log_file" | sort | uniq -c | sort -nr | head -n 5)
+top_requests_by_host=$(awk -F ' ' '{print $13}' "$log_file" |sort| uniq -c | sort -nr | head -n 5)
+top_requests_by_body_bytes_sent=$(awk -F ' ' '{print $9}' "$log_file" | sort -n | tail -n 5)
+top_requests_by_path=$(awk -F ' ' '{print $4}' "$log_file" | cut -d '/' -f 1,2 | sort | uniq -c | sort -nr | head -n 5)
+top_requests_by_response_time=$(awk -F ' ' '{print $7}' "$log_file" | sort -n | tail -n 5)
 
-top_5xx_requests_per_host=$(awk -F ',' '$6 ~ /^5[0-9][0-9]$/{print $13}' "$log_file" | sort | uniq -c | sort -nr | head -n 5)
-requests_within_10_minutes=$(awk -F ',' -v threshold="$time_threshold" '$2 > threshold{print}' "$log_file")
+top_5xx_requests_per_host=$(awk -F ' ' '$6 ~ /^5[0-9][0-9]$/{print $13}' "$log_file" | sort | uniq -c | sort -nr | head -n 5)
+requests_within_10_minutes=$(awk -F ' ' -v threshold="$time_threshold" '$2 > threshold{print}' "$log_file")
 echo "$top_upstream_ip"
 
 echo "$top_requests_by_host"
